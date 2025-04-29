be584cec7dedb71fc64dc8e39285c13beb149040
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-04-29 11:46:53 -0700

    Update App.css

--- src/components/ReminderList.tsx
+++ src/components/ReminderList.tsx
@@ -8,7 +8,7 @@ function ReminderList({items, onRemoveReminder}: ReminderListProps) {
     return (
         <ul className="list-group">
             {items.map(item => 
-            <li className="list-group-item d-flex" key={item.id}>
+            <li className="list-group-item d-flex justify-content-between aluign-items-center" key={item.id}>
                 {item.title}
                 <button onClick={() => onRemoveReminder(item.id)} className="btn btn-outline-danger mx-2 rounded-pill">Delete</button>
             </li>)}
